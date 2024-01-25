480d0306ed6a022b68418f0c7c2ab793d734c878
Ichigo21pro <aiglegon@gmail.com>
2024-01-25 18:58:39 +0000

    Interfaz de monedas

--- Mario Bros 2D/Assets/Script/Coin/ControladorCoin.cs
+++ Mario Bros 2D/Assets/Script/Coin/ControladorCoin.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ControladorCoin : MonoBehaviour
 {
-    public Text contadorText; // Asigna un objeto de tipo Text desde el inspector
+    public TextMeshProUGUI contadorText; // Asigna un objeto de tipo Text desde el inspector
     private int contador = 0;
 
     private void Start()
