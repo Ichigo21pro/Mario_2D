2550c7d70f311294ce642c62304b14955a6f2a1a
Ichigo21pro <aiglegon@gmail.com>
2024-03-05 17:47:48 +0000

    Control de tp Mejorado

--- Mario Bros 2D/Assets/Script/Pipe/PipeTP.cs
+++ Mario Bros 2D/Assets/Script/Pipe/PipeTP.cs
@@ -4,46 +4,37 @@ using UnityEngine;
 
 public class Tp_pipe : MonoBehaviour
 {
-   
+
     public MarioController marioController; // Asegúrate de asignar la referencia de MarioController desde el Inspector
-    
     public GameObject TP;
 
-    private bool tocandose =false;
+    private bool tocandose = false;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             tocandose = true;
-           
         }
-        
     }
+
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             tocandose = false;
-         
-
         }
-
     }
 
     private void Update()
     {
-        if (tocandose && Input.GetKey(KeyCode.DownArrow)) {
-
+        if (tocandose && Input.GetKeyDown(KeyCode.DownArrow))
+        {
             // Teleporta a Mario al TP2
             marioController.Teleport(TP.transform.position);
-            tocandose = false;
-           
+            Debug.Log("Se tepea");
         }
-
     }
-
-
 
 
 
